82be330796ad314723c6e2fa86373249e8e2b9a7
ameliahg <ameliahoovergreen@gmail.com>
2022-08-26 13:46:28 -0400

    adding zip and county data

--- analysis/src/explore.R
+++ analysis/src/explore.R
@@ -1,20 +1,35 @@
 ### explore.R ###
-### basic descriptives and other fun stuff
-### for CalFresh data
+### exploring CalFresh data
 ### AHG
 ### draft of 2022-08-26
 
 stopifnot(basename(getwd())=='analysis')
 
+ # ^ checking to make sure we're running from the main task directory
+
 library(tidyverse)
 library(RCurl)
 
-data_url <- "https://raw.githubusercontent.com/ameliahg/calfresh/main/analysis/input/exercise_data.csv?token=GHSAT0AAAAAABXKMY7XQADJJ2GBEAYEZT36YYI7YSA"
+irs <- read_csv('input/irs_data_CA.csv') %>% # downloaded from IRS website
+  filter(zipcode!=0) %>%
+  group_by(zipcode) %>%
+  mutate(wts=N1/sum(N1,na.rm=TRUE)) %>%
+  summarize(returns=sum(N1,na.rm=TRUE), # proxy for households
+            pop=sum(N2,na.rm=TRUE), # sum of individuals on returns
+            mean_hh_inc_cat=weighted.mean(agi_stub,wts,na.rm=TRUE), 
+            # mean of truncated categories is OK proxy for median
+            mean_hh_inc=weighted.mean(A02650,wts,na.rm=TRUE),
+            mortgages=sum(N19300,na.rm=TRUE),
+            n_hh_under25k=N1[agi_stub==1],
+            n_hh_over200k=N1[agi_stub==6]) %>%
+  mutate(zip=as.character(zipcode),
+         pct_hh_under25k=n_hh_under25k/returns,
+         pct_hh_over200k=n_hh_over200k/returns,
+         pct_with_mortgage=mortgages/returns) %>%
+  ungroup()
 
-cf <- read_csv(data_url) %>% 
+cf <- read_csv('input/exercise_data.csv') %>% 
   mutate(app_id=as.character(app_id),
-         zip=as.character(zip))
-  
-stargazer(as.data.frame(cf),
-          summary=TRUE,type="html",
-          out="output/calfresh_data.html")
+         zip=as.character(zip)) %>%
+  left_join(.,irs)
+
