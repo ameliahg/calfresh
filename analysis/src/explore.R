9b6fc83ae2896596c3d05f81f49bb9762ebde790
ameliahg <ameliahoovergreen@gmail.com>
2022-08-26 13:18:24 -0400

    adding zip data

--- analysis/src/explore.R
+++ analysis/src/explore.R
@@ -1,4 +1,20 @@
 ### explore.R ###
 ### basic descriptives and other fun stuff
 ### for CalFresh data
+### AHG
+### draft of 2022-08-26
 
+stopifnot(basename(getwd())=='analysis')
+
+library(tidyverse)
+library(RCurl)
+
+data_url <- "https://raw.githubusercontent.com/ameliahg/calfresh/main/analysis/input/exercise_data.csv?token=GHSAT0AAAAAABXKMY7XQADJJ2GBEAYEZT36YYI7YSA"
+
+cf <- read_csv(data_url) %>% 
+  mutate(app_id=as.character(app_id),
+         zip=as.character(zip))
+  
+stargazer(as.data.frame(cf),
+          summary=TRUE,type="html",
+          out="output/calfresh_data.html")
